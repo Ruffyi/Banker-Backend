2174344d1ab21ec94364199675f357e0e4cc7b81
Kamil <kam.nas21@wp.pl>
2022-05-19 16:38:43 +0200

    Add a morgan middleware to project, add npm scripts to run server in dev/prod env

--- server.ts
+++ server.ts
@@ -1,11 +1,22 @@
 import express, { Application } from 'express';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './config/.env' });
 
 const app: Application = express();
 
+if (process.env.SERVER_MODE === 'development') {
+	app.use(morgan('dev'));
+}
+
 const SERVER_PORT = process.env.SERVER_PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send({
+		message: 'Hi!',
+	});
+});
 
 app.listen(SERVER_PORT, () => {
 	console.log(`Server is listening at port : {${SERVER_PORT}}`);
