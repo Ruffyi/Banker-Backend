f6f61bbb7ae233ba8f90172d907b68fdbafc1669
Kamil <kam.nas21@wp.pl>
2022-05-19 19:55:25 +0200

    Add a user model with ts types

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,9 +1,12 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import asyncHandler from 'express-async-handler';
 
-const signup = (req: Request, res: Response) => {
-	res.status(200).send({
-		message: 'Hi',
-	});
-};
+const signup = asyncHandler(
+	async (req: Request, res: Response, next: NextFunction) => {
+		res.status(200).send({
+			message: 'Hi',
+		});
+	}
+);
 
 export { signup };
---
+++ models/user.model/user.model.ts
@@ -0,0 +1,35 @@
+import { Schema, Model, model } from 'mongoose';
+import validator from 'validator';
+import IUser from './user.model.interface';
+
+const userSchema = new Schema<IUser>({
+	email: {
+		type: String,
+		required: [true, 'Provide a email address!'],
+		unique: true,
+		lowercase: true,
+		validate: [validator.isEmail, 'Provide a valid email address!'],
+	},
+	password: {
+		type: String,
+		minlength: 6,
+		trim: true,
+		required: [true, 'Provide a password!'],
+	},
+	passwordConfirm: {
+		type: String,
+		minlength: 6,
+		trim: true,
+		required: [true, 'Confirm a password!'],
+		validate: {
+			validator: function (this, password: string) {
+				return this.password === password;
+			},
+			message: 'Passwords must be the same!',
+		},
+	},
+});
+
+const User: Model<IUser> = model('User', userSchema);
+
+export default User;
