581db0c4ff6b29668fc2b16f05f9b711078c6090
Kamil <kam.nas21@wp.pl>
2022-05-22 21:44:50 +0200

    Add a better error responding

--- controllers/error.controller.ts
+++ controllers/error.controller.ts
@@ -8,6 +8,8 @@ dotenv.config({ path: './../config/.env' });
 const sendErrorDev = (err: CustomError, res: Response) => {
 	const { message, status = 'fail', statusCode = 500, stack } = err;
 
+	console.log(err);
+
 	res.status(statusCode).send({
 		error: {
 			message,
@@ -28,6 +30,23 @@ const sendErrorProd = (err: CustomError, res: Response) => {
 	});
 };
 
+const handleDuplicateMongoDB = (err: CustomError) => {
+	const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+	const message = `Duplicate field value: ${value}. Please use another value!`;
+	return new CustomError(message, 400);
+};
+const handleCastErrorDB = (err: CustomError) => {
+	const message = `Invalid ${err.path}: ${err.value}.`;
+	return new CustomError(message, 400);
+};
+
+const handleValidationErrorDB = (err: CustomError) => {
+	const errors = Object.values(err.errors).map((el: any) => el.message);
+
+	const message = `Invalid input data. ${errors.join('. ')}`;
+	return new CustomError(message, 400);
+};
+
 const notFound = (req: Request, res: Response, next: NextFunction) => {
 	return next(new CustomError(`Server can't handle ${req.originalUrl}`, 404));
 };
@@ -38,15 +57,20 @@ const globalErrorMiddleware = (
 	res: Response,
 	next: NextFunction
 ) => {
+	let error = { ...err };
+	if (err.code === 11000) {
+		error = handleDuplicateMongoDB(err);
+	}
+	if (err.name === 'CastError') error = handleCastErrorDB(error);
+	if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
+
 	if (process.env.SERVER_MODE === 'development') {
-		sendErrorDev(err, res);
+		sendErrorDev(error, res);
 	}
 
 	if (process.env.SERVER_MODE === 'production') {
-		sendErrorProd(err, res);
+		sendErrorProd(error, res);
 	}
-
-	console.log(err);
 };
 
 export { globalErrorMiddleware, notFound };
--- utils/CustomError.ts
+++ utils/CustomError.ts
@@ -1,5 +1,10 @@
 export class CustomError extends Error {
 	status: string;
+	code!: number;
+	errmsg!: any;
+	path!: string;
+	value!: string;
+	errors!: any;
 	constructor(public message: string, public statusCode: number) {
 		super(message);
 		this.status = String(statusCode).startsWith('4') ? 'error' : 'fail';
